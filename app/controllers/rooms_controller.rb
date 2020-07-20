602c8622398c67ea297a0490e9f5abb59c872f1c
yutaiko <yuta.ashikawa1984@gmail.com>
2020-07-20 18:09:23 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -13,6 +13,12 @@ class RoomsController < ApplicationController
   def index
   end
 
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
   private
 
   def room_params
